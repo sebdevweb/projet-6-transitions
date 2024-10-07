0aa786316640e9158a184a1a23a64fc0193aea6e
sebdevweb <spansini@squad.fr>
2024-10-07 15:51:45 +0200

    [PROJET-6-TRANSITIONS] Add transition group

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TRANSITIONS
 1. Add simple transition with component
+2. Add TransitionGroup
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <script setup>
   import { ref } from 'vue'
   import FadeSlideTransition from './components/FadeSlideTransition.vue';
+  import { shuffleArray } from '@/composable/shuffleArray';
 
   const showSpoiler = ref(false)
   const toggleSpoiler = () => showSpoiler.value = !showSpoiler.value
+
+  const movies = ref([
+    "Les évadés",
+    "Le parrain",
+    "Batman",
+    "Pulp Fiction",
+    "Forrest Gump",
+    "Inception"
+  ])
+
+  const movie = ref('')
+  const addMovie = () => {
+    movies.value = [movie.value, ...movies.value]
+    movie.value = ''
+  }
+  const removeMovie = (movie) => {
+    movies.value = movies.value.filter(m => m !== movie)
+  }
+  const randomize = () => {
+    movies.value = shuffleArray(movies.value)
+  }
 
 </script>
 
@@ -18,6 +40,17 @@
         <p>Voir une information qui gâche tout.</p>
       </div>
     </FadeSlideTransition>
+    <input type="text" v-model="movie" placeholder="Ajouter un film">
+    <button :disabled="movie.length === 0" @click="addMovie">Ajouter</button>
+    <ul>
+      <TransitionGroup name="list">
+        <li v-for="movie in movies" :key="movie">
+          {{ movie }}
+          <button class="secondary" @click="removeMovie(movie)">X</button>
+        </li>
+      </TransitionGroup>
+    </ul>
+    <button @click="randomize">Réorganiser</button>
   </div>
 </template>
 
@@ -52,15 +85,29 @@
       }
       .spoiler {
         margin-top: 2rem;
-        background-color: lightcoral;
+        background-color: darkred;
         padding: 2rem;
         max-width: 600px;
         border-radius: 20px;
         transition: .5s;
+        margin-bottom: 2rem;
         p {
           color: white;
           font-size: 20px;
         }
+      }
+      .list-enter-active,
+      .list-leave-active,
+      .list-move {
+        transition: .5s ease;
+      }
+      .list-leave-active{
+        position: absolute;
+      }
+      .list-enter-from,
+      .list-leave-to {
+        opacity: 0;
+        transform: translateX(30px);
       }
     }
   }
